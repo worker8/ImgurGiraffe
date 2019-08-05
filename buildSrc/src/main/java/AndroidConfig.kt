0c8d596a98e57ef607a6b3292f5b8a21b1f360a6
Tan Jun Rong <tanjunrong@gmail.com>
2019-08-05 14:18:45 +0900

    first commit

---
+++ buildSrc/src/main/java/AndroidConfig.kt
@@ -0,0 +1,7 @@
+class AndroidConfig {
+    val sdkVersion = 28 // target & compile sdk
+    val minSdkVersion = 21
+    val targetSdkVersion = 28
+    val versionCode = 1
+    val versionName = "0.0.1"
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,36 @@
+object Versions {
+    val Tools = Tool()
+    val AndroidConfigs = AndroidConfig()
+
+    const val androidX = "1.1.0-rc01"
+    const val recyclerView = "1.1.0-beta01"
+    const val constraintLayout = "2.0.0-beta2"
+    const val cyanea = "1.0.1"
+    const val dagger = "2.21"
+    const val espressoCore = "3.0.2"
+    const val fuel = "2.0.1"
+    const val glide = "4.9.0"
+    const val headerFooterAdapter = "0.0.3"
+    const val jraw = "0.9.0"
+    const val jrawAndroid = "v1.0.0"
+    const val junit = "4.12"
+    const val lifecycleEx = "2.2.0-alpha02"
+    const val mockk = "1.8.13.kotlin13"
+    const val paging = "2.1.0"
+    const val pagingRx = "1.0.0-rc1"
+    const val material = "1.1.0-alpha09"
+    const val result = "2.0.0"
+    const val retrofit = "2.4.0"
+    const val rxAndroid = "2.1.0"
+    const val rxBinding = "3.0.0-alpha2"
+    const val rxJava = "2.1.13"
+    const val stetho = "1.5.0"
+    const val support = "28.0.0"
+    const val testRunner = "1.0.2"
+
+}
+
+class Tool {
+    val kotlin = "1.3.21"
+    val buildGradle = "3.4.2"
+}
