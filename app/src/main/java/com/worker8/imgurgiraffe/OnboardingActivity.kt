e9f2ae971e3dd81e42c38d17ce902ec48c7ca82a
Tan Jun Rong <tanjunrong@gmail.com>
2019-08-24 22:26:25 +0900

    improve onboarding page

--- app/src/main/java/com/worker8/imgurgiraffe/OnboardingActivity.kt
+++ app/src/main/java/com/worker8/imgurgiraffe/OnboardingActivity.kt
@@ -2,14 +2,21 @@ package com.worker8.imgurgiraffe
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_onboarding.*
+
 
 class OnboardingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding)
 
+        val shake = AnimationUtils.loadAnimation(this, R.anim.shake)
+        obLoginGiraffeImage.startAnimation(shake)
+        obLoginGiraffeImage.setOnClickListener {
+            obLoginGiraffeImage.startAnimation(shake)
+        }
         obLoginTV.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
         }
